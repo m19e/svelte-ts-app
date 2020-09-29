f54ddaa1e5cb8c89a13c7edad3b852c4ed3b4572
m19e <nexus.me7290@gmail.com>
2020-09-29 20:44:40 +0900

    Typing

--- src/Each.svelte
+++ src/Each.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    const persons = [{ name: "Alice" }, { name: "Bob" }, { name: "Carol" }];
+    const persons: { [key: string]: string }[] = [{ name: "Alice" }, { name: "Bob" }, { name: "Carol" }];
 </script>
 
 {#each persons as { name }}
