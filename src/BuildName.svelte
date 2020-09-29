d075f7a7895e35d3b44fde063b4c02942b72d6a1
m19e <nexus.me7290@gmail.com>
2020-09-29 20:15:46 +0900

    Add buildname

---
+++ src/BuildName.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let firstName: string = "Akiha";
+    let lastName: string = "Arisugawa";
+    let fullName: string = `${firstName} ${lastName}`;
+</script>
+
+<main>
+    <input bind:value={firstName} />
+    <input bind:value={lastName} />
+    <div>Hello, {fullName}</div>
+</main>
